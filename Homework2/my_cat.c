65997aa3ab4165fe740137ef1b5c794e9e8f25b9
track394 <track394@yahoo.com>
2018-02-25 13:16:48 -0800

    Got concatenation working

--- Homework2/my_cat.c
+++ Homework2/my_cat.c
@@ -9,6 +9,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+/*
+ * Reads the file at a given path and prints it to stdout.
+ * Returns an error if file could not be found or opened.
+ */
 int readFile(char* filePath) {
   int currChar;
   FILE *file;
@@ -19,18 +23,24 @@ int readFile(char* filePath) {
     }
     fclose(file);
   } else {
-    printf("There was a problem opening the file. Make sure the correct path is given.\n");
+    printf("There was a problem opening the file, ensure the correct path is given.\n");
     return -1;
   }
   return 0;
 }
 
 int main(int argc, char** argv) {
-  if (argc == 1) {
-    printf("Not enough arguments provided.\n");
+  if (argc == 1) { //Need at least one argument
+    printf("No files provided.\n");
     return -1;
-  } else if(argc == 2) {
-     return readFile(argv[1]);
+  } else if(argc == 2) { //Single file was given, so just print it out
+    return readFile(argv[1]);
+  } else { //More than one file given, read each file and concatenate them together
+    for (int i = 1; i < argc; i++) {
+      if (readFile(argv[i]) == -1) {
+        return -1;
+      }
+    }
   }
   return 0;
 }
