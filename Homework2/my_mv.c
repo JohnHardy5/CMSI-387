483fd9ed91f105f5dbd5467224d3709276b66147
track394 <track394@yahoo.com>
2018-02-26 00:16:44 -0800

    Got mv working

--- Homework2/my_mv.c
+++ Homework2/my_mv.c
@@ -23,6 +23,12 @@ int isFile(char* path) {
   return S_ISREG(path_stat.st_mode);
 }
 
+int isDir(char* path) {
+  struct stat path_stat;
+  stat(path, &path_stat);
+  return S_ISDIR(path_stat.st_mode);
+}
+
 int linkFileTo(char* filePath, char* newPath) {
   if (link(filePath, newPath) == -1) {
     printf("There was a problem linking the file to the given path.\n");
@@ -40,25 +46,57 @@ int unlinkFileFrom(char* filePath) {
 }
 
 int moveDir(char* s, char* d) {
-  d = strcat(d, "/");//Setup destination for new directory
-  d = strcat(d, basename(s));
-  if (mkdir(d, 0777) == -1) {
-    printf("Directory could not be moved.\n");
+  char* tempS = malloc(sizeof(char) * BUFFER_SIZE);
+  char* tempD = malloc(sizeof(char) * BUFFER_SIZE);
+  strncpy(tempS, s, BUFFER_SIZE);
+  strncpy(tempD, d, BUFFER_SIZE);
+  tempD = strcat(tempD, "/");//Setup destination for new directory
+  tempD = strcat(tempD, basename(tempS));
+  //printf("%s\n", tempD);
+  if (mkdir(tempD, 0777) == -1) {
+    printf("New directory could not be created.\n");
     return -1;
   }
-  DIR* src = opendir(s);
-  struct dirent* currItem;
+
+  DIR* src = opendir(tempS);
   if (src == NULL) {
     printf("Source directory could not be opened.\n");
     return -1;
   }
+
+  struct dirent* currItem;
   while ((currItem = readdir(src)) != NULL) {
-    printf("%s\n", currItem->d_name);
+    strncpy(tempS, s, BUFFER_SIZE);
+    strncpy(tempD, d, BUFFER_SIZE);
+    tempD = strcat(tempD, "/");//Setup destination for new directory
+    tempD = strcat(tempD, basename(tempS));
+    char* name = currItem->d_name;
+    if (strcmp(name, ".") == 0 || strcmp(name, "..") == 0) {
+      continue;
+    }
+    char* currPath = strcat(tempS, "/");
+    currPath = strcat(currPath, name);
+    if (isFile(currPath)) {
+      char* newPath = strcat(tempD, "/");
+      newPath = strcat(newPath, name);
+      printf("Found file: %s\n", currPath);
+      printf("Linking to dir: %s\n", newPath);
+      linkFileTo(currPath, newPath);
+      unlinkFileFrom(currPath);
+    } else if (isDir(currPath)){//Otherwise repeat on the sub-directory
+      printf("Found directory: %s\n", currPath);
+      moveDir(currPath, tempD);
+    }
   }
+
+  rmdir(s);//All files moved, delete old folder
+
   if (closedir(src) == -1) {
     printf("Source directory could not be closed.\n");
     return -1;
   }
+  free(tempS);
+  free(tempD);
   return 0;
 }
 
