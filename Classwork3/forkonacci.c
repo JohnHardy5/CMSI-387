35f18e7afe64c9939b01332a479de172b5fc4463
track394 <track394@yahoo.com>
2018-02-27 19:03:29 -0800

    Finished classwork3

--- Classwork3/forkonacci.c
+++ Classwork3/forkonacci.c
@@ -1,6 +1,10 @@
+/*
+ * Written mostly by John Hardy, insight provided by J Goocher. Joey Martinez
+ * and Jackson Watkins kind of helped a little.
+ * WARNING: THIS ONLY WORKS UP TO THE 93rd Fib Number
+ */
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
 
@@ -9,17 +13,31 @@ int main(int argc, char** argv) {
     printf("Not enough args given.\n");
     return -1;
   }
+
   int n = atoi(argv[1]);
-  printf("The Fibonacci sequence using forks is:");
-
-  pid_t pid;
-  pid = fork();
-
-  if (pid == 0) {
-
-  } else {
-
+  if (n < 1 || n > 93) {
+    printf("What are you doing? Stop that!\n");
+    return -1;
   }
+  long currVal = 0;
+  long nextVal = 1;
 
+  for (int i = 0; i < n; i++) {
+    pid_t pid;
+    pid = fork();
+    wait(NULL);
+
+    if (pid == 0) {//Child prints values
+      printf(" %ld", currVal);
+      if (i == n - 1) {
+        printf("\n");
+      }
+      return 0;
+    } else {//Parent produces values
+      long temp = currVal;
+      currVal = nextVal;
+      nextVal = temp + nextVal;
+    }
+  }
   return 0;
 }
