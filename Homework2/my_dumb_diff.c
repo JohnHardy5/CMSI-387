5392db506481d76f8addd72013ad8a48e2dce51a
track394 <track394@yahoo.com>
2018-02-28 16:02:24 -0800

    Abstracted path validation

--- Homework2/my_dumb_diff.c
+++ Homework2/my_dumb_diff.c
@@ -5,10 +5,25 @@
  */
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
 #include <stdio.h>//TODO: Remove printfs and stdio library
 
 static const int BUFFER_SIZE = 1024;
+
+int isFile(char* path) {
+  struct stat path_stat;
+  stat(path, &path_stat);
+  return S_ISREG(path_stat.st_mode);
+}
+
+char* loadFilePath(char* p) {
+  char* newPath = malloc(sizeof(char) * BUFFER_SIZE);
+  if (realpath(p, newPath) == NULL || !isFile(newPath)) {
+    return NULL;
+  }
+  return newPath;
+}
 
 int main(int argc, char** argv) {
   if (argc < 3) {
@@ -16,15 +31,15 @@ int main(int argc, char** argv) {
     exit(-1);
   }
 
-  char* pathOne = malloc(sizeof(char) * BUFFER_SIZE);
-  if (realpath(argv[1], pathOne) == NULL) {
-    write(2, "First argument was not a path.\n", 31);
+  char* pathOne = loadFilePath(argv[1]);
+  if (pathOne == NULL) {
+    write(2, "First argument was not valid.\n", 30);
     exit(-1);
   }
 
-  char* pathTwo = malloc(sizeof(char) * BUFFER_SIZE);
-  if (realpath(argv[2], pathTwo) == NULL) {
-    write(2, "Second argument was not a path.\n", 32);
+  char* pathTwo = loadFilePath(argv[2]);
+  if (pathTwo == NULL) {
+    write(2, "Second argument was not a valid.\n", 33);
     exit(-1);
   }
 
